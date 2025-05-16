f8dd31b4022825e5c0868b002229780250c872a1
yngpiu <anhduc08768@gmail.com>
2025-05-17 03:53:36 +0700

    feat: Enable Firebase offline persistence
    This commit enables Firebase Realtime Database offline persistence.
    
    - Created `MyApp.java`, an `Application` subclass, to initialize Firebase persistence by calling `FirebaseDatabase.getInstance().setPersistenceEnabled(true)` in its `onCreate()` method.
    - Updated `AndroidManifest.xml` to register `MyApp` as the application class using `android:name=".MyApp"`.
    - Corrected the `android:name` attribute for `MainActivity` in `AndroidManifest.xml` to `".ui.MainActivity"`.

---
+++ app/src/main/java/com/haui/noteapp/MyApp.java
@@ -0,0 +1,18 @@
+package com.haui.noteapp;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        Log.d(
+                "Firebase",
+                "Firebase persistence enabled"
+        );
+    }
+}
